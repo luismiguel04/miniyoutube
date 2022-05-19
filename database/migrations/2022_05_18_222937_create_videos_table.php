23c4bd1323b5e5fe66cf84c15dc06ada88fa4748
luismiguel04 <2120100449@soy.utj.edu.mx>
2022-05-19 16:52:26 -0500

    Primer commit

---
+++ database/migrations/2022_05_18_222937_create_videos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('titulo', 150);
+            $table->string('descripcion', 250);
+            $table->string('image','150');
+            $table->string('video_path');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
