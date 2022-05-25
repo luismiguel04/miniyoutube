69ce21f7b0d55a4a5b831910b1f58b5b9c618c37
luismiguel04 <2120100449.soy.utj.edu.mx>
2022-05-25 22:55:54 +0100

    se trabajo modelos de vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+                            <h1>hola</h1>
                         </div>
                     @endif
 
